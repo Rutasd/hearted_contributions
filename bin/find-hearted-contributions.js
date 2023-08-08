c6ed88227643c3df1f8d435cb5967a664b753acb
Rutasd <rutasdeshpande@gmail.com>
2023-08-08 15:40:15 -0400

    csv added

--- bin/find-hearted-contributions.js
+++ bin/find-hearted-contributions.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
-
+import csvWriter from "csv-writer";
 import findHeartedContributions from "../index.js";
 
 var argv = yargs
   .usage("Usage: $0 [options]")
   .example(
-    "$0 --token 0123456789012345678901234567890123456789 --in https://github.com/octokit --by gr2m --since 2019-05-01"
+    "$0 --token 0123456789012345678901234567890123456789 --in https://github.com/octokit --since 2019-05-01"
   )
-
   .options({
     token: {
       description:
@@ -22,11 +21,6 @@ var argv = yargs
       demandOption: true,
       type: "string",
     },
-    // by: {
-    //   description: "GitHub username (or comma-separated list of usernames",
-    //   demandOption: true,
-    //   type: "string",
-    // },
     since: {
       description:
         "timestamp in ISO 8601 format or GitHub issue URL, in which case since will be set to the created_at timestamp of the last comment",
@@ -41,16 +35,42 @@ var argv = yargs
   })
   .epilog("copyright 2019").argv;
 
-const heartedItems = await findHeartedContributions(argv);
+// Create a CSV writer instance
+const writer = csvWriter.createObjectCsvWriter({
+  path: 'heartedItems.csv',
+  header: [
+    { id: 'url', title: 'URL' }
+  ]
+});
+
+// Placeholder for all hearted items
+const allHeartedItems = [];
+
+// Placeholder for getting all users - Modify this based on your implementation
+const allUsers = getAllUsers();
+
+for (const user of allUsers) {
+  const heartedItems = await findHeartedContributions({
+    ...argv,
+    by: user
+  });
+
+  allHeartedItems.push(...heartedItems.map(url => ({ url })));
+}
+
+// Write to CSV
+await writer.writeRecords(allHeartedItems);
 
 console.log("\n\ndone.\n");
 
-if (heartedItems.length === 0) {
+if (allHeartedItems.length === 0) {
   console.log("No hearted items.");
 }
 
-console.log("hearted items:");
+console.log("hearted items written to CSV file.");
 
-for (const url of heartedItems.sort()) {
-  console.log(`♥️ ${url}`);
+// Function to retrieve all users 
+function getAllUsers() {
+  
+  return ["gr2m"]; // Example return
 }
